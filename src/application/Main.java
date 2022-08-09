3fe4d17b7911c91fed08c02aa38842fbcbfa6ec4
Daniel Almeida <47141490+dcalmeida149@users.noreply.github.com>
2022-08-08 22:04:59 -0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,21 @@
 package application;
 
+
 import java.util.Locale;
 import java.util.Scanner;
+
 import entities.Triangle;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
+		
 		Scanner sc = new Scanner(System.in);
-
-		Triangle x, y;
-
-		x = new Triangle();
-		y = new Triangle();
+		Triangle x = new Triangle();
+		Triangle y = new Triangle();
+		
+		System.out.println("Comparar a Area de dois triangulos e indicar qual é o maior.");
 
 		System.out.println("Entre as medidas do triangulo X: ");
 		
@@ -27,15 +29,11 @@ public class Main {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-		
-		p = (y.a + y.b + y.c) / 2.0;
-		
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("Area do Triangulo X: %.4f%n", areaX);
-		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		System.out.printf("Area do Triangulo Y: %.4f%n", areaY);
 		
 		if (areaX > areaY) {
 			System.out.println("Larger area: X");
